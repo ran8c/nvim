565616a5a5ae822611062b6fc41711d8795af169
ran8c <ran8c@icloud.com>
2025-05-24 11:32:45 -0700

    feat(lsp): Add clangd.lua

---
+++ lsp/clangd.lua
@@ -0,0 +1,12 @@
+return {
+	cmd = {
+		"clangd",
+		"--background-index",
+	},
+	root_markers = {
+		"compile_commands.json",
+		"compile_flags.txt",
+		".git",
+	},
+	filetypes = { "c", "cpp" },
+}
