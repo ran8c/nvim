150701fd9775e7726975e828845361acb2a7bf77
ran8c <ran8c@icloud.com>
2025-05-24 10:29:07 -0700

    feat: Add built-in vim settings

---
+++ init.lua
@@ -0,0 +1,18 @@
+vim.g.mapleader = " "
+
+-- built-in vim settings
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+
+vim.o.textwidth = 79
+vim.o.colorcolumn = "+1"
+
+vim.o.expandtab = false
+vim.o.tabstop = 4
+vim.o.softtabstop = 0
+vim.o.shiftwidth = 0
+
+vim.o.hlsearch = false
+vim.o.ignorecase = true
+vim.o.smartcase = true
