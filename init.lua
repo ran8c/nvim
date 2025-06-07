288303ba5a8143ccc98595e840ba0cd56d399a3f
ran8c <ran8c@icloud.com>
2025-06-07 12:51:09 -0700

    feat: Enable vim-surround map for mini.surround

--- init.lua
+++ init.lua
@@ -45,7 +45,23 @@ require("mini.align").setup({
 })
 
 -- mini.surround: manage delimiters
-require("mini.surround").setup()
+require("mini.surround").setup({
+	mappings = {
+		add = "ys",
+		delete = "ds",
+		find = "",
+		find_left = "",
+		highlight = "",
+		replace = "cs",
+		update_n_lines = "",
+		suffix_last = "",
+		suffix_next = "",
+	},
+	search_method = "cover_or_next",
+})
+vim.keymap.del("x", "ys")
+vim.keymap.set("x", "S", [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+vim.keymap.set("n", "yss", "ys_", { remap = true })
 
 -- mini.clue: show available keymaps
 local miniclue = require("mini.clue")
